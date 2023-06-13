bea5cdfcad08501b016b6ba4a73a4e4d98401951
Ghassen-bgh <3591@holbertonstudents.com>
2023-06-13 17:12:16 +0200

    matrix add

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""matrix addition"""
+
+
+def add_arrays(arr1, arr2):
+    """add two arrays"""
+    if len(arr1) != len(arr2):
+        return None
+    return [arr1[i] + arr2[i] for i in range(len(arr1))]
